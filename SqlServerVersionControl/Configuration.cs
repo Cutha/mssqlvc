f9bf8e478ffcf5cf7499fa7b96a86b6cce06d7cd
Cutha <yeungcfhk@gmail.com>
2019-04-29 16:22:53 +0800

    First working version

---
+++ SqlServerVersionControl/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerVersionControl
+{
+    class Configuration
+    {
+        public Connection Connection { get; set; }
+        public DateTime? LastSyncTime { get; set; }
+    }
+
+    class Connection
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SqlServerVersionControl/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerVersionControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string configFile = "config.json";
+            if (!File.Exists(configFile))
+            {
+                Console.WriteLine("error: " + configFile + " does not exist");
+                return;
+            }
+            string json = File.ReadAllText(configFile);
+            Configuration config = JsonConvert.DeserializeObject<Configuration>(json);
+            DbContext db = new DbContext(config.Connection.Server, config.Connection.User, config.Connection.Password, config.Connection.Database);
+
+            DateTime syncTime;
+            if (config.LastSyncTime == null)
+                syncTime = db.Load();
+            else
+                syncTime = db.Load(config.LastSyncTime.GetValueOrDefault());
+            db.Sync();
+
+            config.LastSyncTime = syncTime;
+
+            // Serialize config
+            JsonSerializer serializer = new JsonSerializer
+            {
+                ContractResolver = new DefaultContractResolver { NamingStrategy = new CamelCaseNamingStrategy() },
+            };
+            using (StreamWriter sw = new StreamWriter(configFile))
+            using (JsonWriter jw = new JsonTextWriter(sw) {
+                Formatting = Formatting.Indented,
+                Indentation = 4
+            })
+            {
+                serializer.Serialize(jw, config);
+            }
+        }
+    }
+}
